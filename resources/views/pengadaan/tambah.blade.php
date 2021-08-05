b7144b80fc14e889b9094a1b8110bbed96cedb1e
kristiandika <cristiandicka@gmail.com>
2021-08-05 18:28:28 +0700

    Membuat Form Tambah Data Pengadaan Pada Modal (65)

--- resources/views/pengadaan/tambah.blade.php
+++ resources/views/pengadaan/tambah.blade.php
@@ -7,13 +7,38 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
+      <form action="/tambahAdmin" method="post" role="form">
+          {{csrf_field()}}
       <div class="modal-body">
-        ...
+      
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Nama Pengadaan</label>
+                    <input type="text" class="form-control" id="nama_pengadaan" name="nama_pengadaan" placeholder="Masukan Nama Pengadaan">
+                  </div>
+                    <div class="form-group">
+                    <label for="exampleInputEmail1">Deskripsi</label>
+                    <textarea class="form-control" id="deskripsi" name="deskripsi" placeholder="Masukan deskripsi"></textarea>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Gambar</label>
+                    <input type="file" class="form-control" id="gambar" name="gambar" accept="image/png, image/jpg, image/gif">
+                  </div>
+                  <div class="form-group">
+                    <!-- <label for="exampleInputEmail1">Anggaran</label> -->
+                    <label>Anggaran: <input type="" class="" disable style="border:none; background-color: white; color: black;"></label>
+                    <input type="text" class="form-control" id="anggaran" name="anggaran" placeholder="Masukan Anggaran">
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+             
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-        <button type="button" class="btn btn-primary">Simpan</button>
+        <button type="submit" class="btn btn-primary">Simpan Data</button>
       </div>
+      </form>
     </div>
   </div>
 </div>
