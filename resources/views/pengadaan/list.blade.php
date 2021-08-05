3336c61649d63cd6ab5fd2ba6832f1b1c35b6135
kristiandika <cristiandicka@gmail.com>
2021-08-05 18:06:39 +0700

    Membuat Modal Tambah Data Pengadaan

--- resources/views/pengadaan/list.blade.php
+++ resources/views/pengadaan/list.blade.php
@@ -70,6 +70,9 @@
         <!-- /.row -->
         <div class="row">
           <div class="col-12">
+          <div class="text-right mb-2">
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pengadaanModal"><i class="fas fa-plus"></i> Tambah Data</button>
+              </div>
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title">Pengadaan</h3>
@@ -120,7 +123,8 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-    @include('parsial.footer');
+    @include('parsial.footer')
+    @include('pengadaan.tambah')
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
---
+++ resources/views/pengadaan/tambah.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="pengadaanModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Data Pengadaan</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        <button type="button" class="btn btn-primary">Simpan</button>
+      </div>
+    </div>
+  </div>
+</div>
