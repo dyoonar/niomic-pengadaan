c915b8bae97c5dd02311d2c99d8af2b4998c28a0
kristiandika <cristiandicka@gmail.com>
2021-08-18 09:16:22 +0700

    Menukar posisi layout Ajukan Pengajuan Dengan Daftar Pengadaan (108)

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -121,23 +121,6 @@
       </div>
     </section><!-- End Services Section -->
 
-    <!-- ======= Cta Section ======= -->
-    <section id="cta" class="cta">
-      <div class="container" data-aos="zoom-in">
-
-        <div class="row">
-          <div class="col-lg-9 text-center text-lg-start">
-            <h3>Ajukan Pengajuan</h3>
-            <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="col-lg-3 cta-btn-container text-center">
-            <a class="cta-btn align-middle" href="/registrasi">Daftar Sekarang</a>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- End Cta Section -->
-
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
       <div class="container" data-aos="fade-up">
@@ -164,6 +147,22 @@
       </div>
     </section><!-- End Portfolio Section -->
     <div class="d-flex justify-content-center">{{$pengadaan->links()}}</div>
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-start">
+            <h3>Ajukan Pengajuan</h3>
+            <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="/registrasi">Daftar Sekarang</a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
