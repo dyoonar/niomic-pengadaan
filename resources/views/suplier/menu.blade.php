702d58478721a8753aa9eb6eee150472722f86a8
kristiandika <cristiandicka@gmail.com>
2021-08-18 10:29:37 +0700

    Tambah Menu Halaman Depan Dashboard Suplier (110)

--- resources/views/suplier/menu.blade.php
+++ resources/views/suplier/menu.blade.php
@@ -3,6 +3,14 @@
     <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
     <li class="nav-item">
+      <a href="/" class="nav-link">
+        <i class="nav-icon fas fa-home"></i>
+        <p>
+          Halaman Depan
+        </p>
+      </a>
+    </li>
+    <li class="nav-item">
       <a href="/listSuplier" class="nav-link">
         <i class="nav-icon fas fa-list"></i>
         <p>
