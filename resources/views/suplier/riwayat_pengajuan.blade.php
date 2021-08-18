15f01fe9858553f8f0be489ca5b4528953497f0a
kristiandika <cristiandicka@gmail.com>
2021-08-18 10:50:06 +0700

    Ganti Label Laporan Pengajuan -> Pilih File (112)

--- resources/views/suplier/riwayat_pengajuan.blade.php
+++ resources/views/suplier/riwayat_pengajuan.blade.php
@@ -126,9 +126,9 @@
                           <form method="post" action="/tambahLaporan" enctype="multipart/form-data">
                             {{csrf_field()}}
                             <input type="hidden" name="id_pengajuan" id="id_pengajuan" value="{{$p['id_pengajuan']}}">
-                            <label for="laporan" class="btn btn-block btn-outline-info btn-flat">Laporan Pengajuan</label>
+                            <label for="laporan" class="btn btn-block btn-outline-info btn-flat">Pilih File Laporan</label>
                             <input type="file" name="laporan" id="laporan" class="form-control" style="display:none" accept="application/pdf">
-                            <button type="submit" class="btn btn-secondary">Upload</button>
+                            <button type="submit" class="btn btn-secondary">Submit</button>
                           </form>
                           @else
                           <a href="{{asset(Storage::url($p['laporan']))}}" class="btn btn-warning" target="_blank"><i class="fa fa-eye"></i>Lihat Laporan</a>
