407b12bc8c9d8f1a39d3b0f66238da903b7d6b75
kristiandika <cristiandicka@gmail.com>
2021-08-19 13:27:18 +0700

    Membuat View Suplier Admin (118)

--- resources/views/parsial/menu.blade.php
+++ resources/views/parsial/menu.blade.php
@@ -18,7 +18,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a href="" class="nav-link">
+          <a href="/listSup" class="nav-link">
             <i class="far fa-circle nav-icon"></i>
             <p>Suplier</p>
           </a>
